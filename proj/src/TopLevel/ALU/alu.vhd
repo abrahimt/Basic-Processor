ac04389ec5bb256a3ae7e7fcd20837c7bf4f9449
Abrahim Toutoungi <abrahimt@iastate.edu>
2024-03-25 10:12:17 -0500

    ALU stuff

--- proj/src/TopLevel/ALU/alu.vhd
+++ proj/src/TopLevel/ALU/alu.vhd
@@ -5,292 +5,246 @@
 -- Used to perform operations based on the particular instruction
 -------------------------------------------------------------------------
 
-LIBRARY IEEE; -- Library declaration for IEEE standard
-USE IEEE.std_logic_1164.ALL; -- Importing standard logic data types
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
 
-ENTITY alu IS -- Entity declaration for the ALU module
-    PORT (
-        i_RS, i_RT : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input ports for operands
-        i_Imm : IN STD_LOGIC_VECTOR(15 DOWNTO 0); -- Input port for immediate value
-        i_ALUOp : IN STD_LOGIC_VECTOR(3 DOWNTO 0); -- Input port for ALU operation code
-        i_ALUSrc : IN STD_LOGIC; -- Input port for ALU source selection
-        i_bne : IN STD_LOGIC; -- Input port for branch if not equal
-        i_beq : IN STD_LOGIC; -- Input port for branch if equal
-        i_shiftDir : IN STD_LOGIC; -- Input port for shift direction
-        i_shiftType : IN STD_LOGIC; -- Input port for shift type
-        i_shamt : IN STD_LOGIC_VECTOR(4 DOWNTO 0); -- Input port for shift amount
-        i_addSub : IN STD_LOGIC; -- Input port for add/subtract operation
-        i_signed : IN STD_LOGIC; -- Input port for signed operation
-        i_lui : IN STD_LOGIC; -- Input port for load upper immediate
-        o_result : OUT STD_LOGIC_VECTOR(31 DOWNTO 0); -- Output port for result
-        o_overflow : OUT STD_LOGIC; -- Output port for overflow flag
-        o_zero : OUT STD_LOGIC -- Output port for branch signal
-    );
-END alu; -- End of entity declaration
+ENTITY alu IS
+	PORT (
+		i_RS, i_RT : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+		i_Imm : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+		i_ALUOp : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		i_ALUSrc : IN STD_LOGIC;
+		i_bne : IN STD_LOGIC;
+		i_beq : IN STD_LOGIC;
+		i_shiftDir : IN STD_LOGIC;
+		i_shiftType : IN STD_LOGIC;
+		i_shamt : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+		i_addSub : IN STD_LOGIC;
+		i_signed : IN STD_LOGIC;
+		i_lui : IN STD_LOGIC;
+		o_result : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+		o_overflow : OUT STD_LOGIC;
+		o_branch : OUT STD_LOGIC);
 
-ARCHITECTURE structure OF alu IS -- Architecture declaration for the structural modeling
+END alu;
 
-    -- Component declarations for various submodules used in the ALU
+ARCHITECTURE structure OF alu IS
 
-    COMPONENT nBitAddSub IS
-        PORT (
-            input_A, input_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input ports for operands A and B
-            nAdd_Sub : IN STD_LOGIC; -- Input port for add/subtract operation
-            output_S : OUT STD_LOGIC_VECTOR(31 DOWNTO 0); -- Output port for result
-            output_C : OUT STD_LOGIC; -- Output port for carry-out
-            o_Overflow : OUT STD_LOGIC -- Output port for overflow flag
-        );
-    END COMPONENT;
+	COMPONENT nBitAddSub IS
+		PORT (
+			input_A, input_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			nAdd_Sub : IN STD_LOGIC;
+			output_S : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+			output_C : OUT STD_LOGIC;
+			o_Overflow : OUT STD_LOGIC);
+	END COMPONENT;
 
-    COMPONENT nBitOr IS
-        PORT (
-            i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand A
-            i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand B
-            o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- Output port for result
-        );
-    END COMPONENT;
+	COMPONENT nBitOr IS
+		PORT (
+			i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	END COMPONENT;
 
-    COMPONENT nBitAnd IS
-        PORT (
-            i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand A
-            i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand B
-            o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- Output port for result
-        );
-    END COMPONENT;
+	COMPONENT nBitAnd IS
+		PORT (
+			i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	END COMPONENT;
 
-    COMPONENT nBitNor IS
-        PORT (
-            i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand A
-            i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand B
-            o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- Output port for result
-        );
-    END COMPONENT;
+	COMPONENT nBitNor IS
+		PORT (
+			i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	END COMPONENT;
 
-    COMPONENT nBitXor IS
-        PORT (
-            i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand A
-            i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand B
-            o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- Output port for result
-        );
-    END COMPONENT;
+	COMPONENT nBitXor IS
+		PORT (
+			i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	END COMPONENT;
 
-    COMPONENT setLessThan IS
-        PORT (
-            i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand A
-            i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand B
-            o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- Output port for result
-        );
-    END COMPONENT;
+	COMPONENT setLessThan IS
+		PORT (
+			i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	END COMPONENT;
 
-    COMPONENT branch IS
-        PORT (
-            i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand A
-            i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for operand B
-            i_beq : IN STD_LOGIC; -- Input port for branch if equal
-            i_bne : IN STD_LOGIC; -- Input port for branch if not equal
-            o_branchFlag : OUT STD_LOGIC -- Output port for branch flag
-        );
-    END COMPONENT;
+	COMPONENT branch IS
+		PORT (
+			i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_beq : IN STD_LOGIC;
+			i_bne : IN STD_LOGIC;
+			o_branchFlag : OUT STD_LOGIC);
+	END COMPONENT;
 
-    COMPONENT Barrel_Shifter IS
-        PORT (
-            i_shamt : IN STD_LOGIC_VECTOR(4 DOWNTO 0); -- Input port for shift amount
-            i_sign : IN STD_LOGIC; -- Input port for sign bit
-            i_leftShift : IN STD_LOGIC; -- Input port for left shift operation
-            i_D : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for data to be shifted
-            o_O : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- Output port for shifted data
-        );
-    END COMPONENT;
+	COMPONENT Barrel_Shifter IS
+		PORT (
+			i_shamt : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+			i_sign : STD_LOGIC;
+			i_leftShift : STD_LOGIC;
+			i_D : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			o_O : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	END COMPONENT;
 
-    COMPONENT mux2t1_N IS
-        PORT (
-            i_S : IN STD_LOGIC; -- Input port for select signal
-            i_D0 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for data 0
-            i_D1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for data 1
-            o_O : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- Output port for selected data
-        );
-    END COMPONENT;
+	COMPONENT mux2t1_N IS
+		PORT (
+			i_S : IN STD_LOGIC;
+			i_D0 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_D1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			o_O : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	END COMPONENT;
 
-    COMPONENT mux2t1_5bit IS
-        PORT (
-            i_S : IN STD_LOGIC; -- Input port for select signal
-            i_D0 : IN STD_LOGIC_VECTOR(4 DOWNTO 0); -- Input port for data 0 (5 bits)
-            i_D1 : IN STD_LOGIC_VECTOR(4 DOWNTO 0); -- Input port for data 1 (5 bits)
-            o_O : OUT STD_LOGIC_VECTOR(4 DOWNTO 0) -- Output port for selected data (5 bits)
-        );
-    END COMPONENT;
+	COMPONENT mux2t1_5bit IS
+		PORT (
+			i_S : IN STD_LOGIC;
+			i_D0 : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+			i_D1 : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+			o_O : OUT STD_LOGIC_VECTOR(4 DOWNTO 0));
+	END COMPONENT;
 
-    COMPONENT andg2 IS
-        PORT (
-            i_A : IN STD_LOGIC; -- Input port A for AND gate
-            i_B : IN STD_LOGIC; -- Input port B for AND gate
-            o_F : OUT STD_LOGIC -- Output port for result
-        );
-    END COMPONENT;
+	COMPONENT andg2 IS
+		PORT (
+			i_A : IN STD_LOGIC;
+			i_B : IN STD_LOGIC;
+			o_F : OUT STD_LOGIC);
+	END COMPONENT;
 
-    COMPONENT xorg2 IS
-        PORT (
-            i_A : IN STD_LOGIC; -- Input port A for XOR gate
-            i_B : IN STD_LOGIC; -- Input port B for XOR gate
-            o_F : OUT STD_LOGIC -- Output port for result
-        );
-    END COMPONENT;
-
-    component Extender is
-        port(
-            i_data       : in std_logic_vector(15 downto 0);
-            o_out        : out std_logic_vector(31 downto 0));
-    end component;
-
-    -- Driven by the ALUOp control signal, this module will decide what operation is output by ALU
-    COMPONENT selectOperation IS
-        PORT (
-            i_ALUOp : IN STD_LOGIC_VECTOR(3 DOWNTO 0); -- Input port for ALU operation code
-            i_orResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for OR operation result
-            i_andResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for AND operation result
-            i_xorResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for XOR operation result
-            i_norResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for NOR operation result
-            i_sltResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for SLT operation result
-            i_addSubResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for ADD/SUB operation result
-            i_shiftResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input port for shift operation result
-            o_result : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) -- Output port for ALU result
-        );
-    END COMPONENT;
-
-    -- Signals Declaration
-    SIGNAL s_Operand : STD_LOGIC_VECTOR(31 DOWNTO 0); -- either RS or Immediate
-    SIGNAL s_orResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL s_andResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL s_xorResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL s_norResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL s_addSubResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL s_addSubCarry : STD_LOGIC;
-    SIGNAL s_sltResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL s_shiftResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    SIGNAL s_barrelShiftSign : STD_LOGIC;
-    SIGNAL s_MSB : STD_LOGIC;
-    SIGNAL s_overflowDetected : STD_LOGIC;
-    SIGNAL s_constShamt : STD_LOGIC_VECTOR(4 DOWNTO 0) := "10000";
-    SIGNAL s_shamt : STD_LOGIC_VECTOR(4 DOWNTO 0);
-
+	COMPONENT xorg2 IS
+		PORT (
+			i_A : IN STD_LOGIC;
+			i_B : IN STD_LOGIC;
+			o_F : OUT STD_LOGIC);
+	END COMPONENT;
+	--Driven by the ALUOp control signal, this module will decide what operation is output by ALU
+	COMPONENT selectOperation IS
+		PORT (
+			i_ALUOp : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+			i_orResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_andResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_xorResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_norResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_sltResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_addSubResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			i_shiftResult : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			o_result : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+	END COMPONENT;
+	SIGNAL s_Operand : STD_LOGIC_VECTOR(31 DOWNTO 0); -- either RS or Immediate
+	SIGNAL s_orResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL s_andResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL s_xorResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL s_norResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL s_addSubResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL s_addSubCarry : STD_LOGIC;
+	SIGNAL s_sltResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL s_shiftResult : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL s_barrelShiftSign : STD_LOGIC;
+	SIGNAL s_MSB : STD_LOGIC;
+	SIGNAL s_overflowDetected : STD_LOGIC;
+	SIGNAL s_constShamt : STD_LOGIC_VECTOR(4 DOWNTO 0) := "10000";
+	SIGNAL s_shamt : STD_LOGIC_VECTOR(4 DOWNTO 0);
 BEGIN
-    s_MSB <= s_addSubResult(31);
+	s_MSB <= s_addSubResult(31);
+	PROCESS (i_RT, i_shiftType) --to determined the value of the sign port in the barrel shifter
+	BEGIN
 
-    -- Process to determine the value of the sign port in the barrel shifter
-    PROCESS (i_RT, i_shiftType)
-    BEGIN
-        IF i_RT(31) = '1' AND i_shiftType = '1' THEN
-            s_barrelShiftSign <= '1'; -- Set the barrel shifter sign to 1 for arithmetic shift
-        ELSE
-            s_barrelShiftSign <= '0'; -- Set the barrel shifter sign to 0 for logical shift
-        END IF;
-    END PROCESS;
+		IF i_RT(31) = '1' AND i_shiftType = '1' THEN --used to determine if which sign the barrel shifter will take on based on whether it is logical or arithmetic
+			s_barrelShiftSign <= '1';
+		ELSE
+			s_barrelShiftSign <= '0';
+		END IF;
 
-    -- Multiplexers and Submodules Instantiation
+	END PROCESS;
 
-    -- Mux to select between ALUSrc and RT/Immediate
-    G_MUX_IMM : mux2t1_N
-    PORT MAP(
-        i_S => i_ALUSrc, -- ALUSrc control signal
-        i_D0 => i_RT, -- RT input
-        i_D1 => s_signImm, -- Immediate input
-        o_O => s_Operand); -- Output selected operand
+	G_MUX_IMM : mux2t1_N
+	PORT MAP(
+		i_S => i_ALUSrc,
+		i_D0 => i_RT,
+		i_D1 => i_Imm,
+		o_O => s_Operand);
 
-    -- Mux to select between LUI Shift Amount and Constant Shift Amount
-    G_MUX_LUI : mux2t1_5bit
-    PORT MAP(
-        i_S => i_lui, -- LUI control signal
-        i_D0 => i_shamt, -- Shift amount for LUI instruction
-        i_D1 => s_constShamt, -- Constant shift amount for other instructions
-        o_O => s_shamt); -- Output selected shift amount
+	G_MUX_LUI : mux2t1_5bit --determines if we shift by shamt (for shift instructions) or by constant 16 for lui instruction
+	PORT MAP(
+		i_S => i_lui,
+		i_D0 => i_shamt,
+		i_D1 => s_constShamt,
+		o_O => s_shamt);
 
-    -- OR gate operation
-    G_OR : nBitOr
-    PORT MAP(
-        i_A => i_RS, -- Input A for OR gate
-        i_B => s_Operand, -- Input B for OR gate
-        o_F => s_orResult); -- Output of the OR gate
+	G_OR : nBitOr
+	PORT MAP(
+		i_A => i_RS,
+		i_B => s_Operand,
+		o_F => s_orResult);
 
-    -- AND gate operation
-    G_AND : nBitAnd
-    PORT MAP(
-        i_A => i_RS, -- Input A for AND gate
-        i_B => s_Operand, -- Input B for AND gate
-        o_F => s_andResult); -- Output of the AND gate
+	G_AND : nBitAnd
+	PORT MAP(
+		i_A => i_RS,
+		i_B => s_Operand,
+		o_F => s_andResult);
 
-    -- XOR gate operation
-    G_XOR : nBitXor
-    PORT MAP(
-        i_A => i_RS, -- Input A for XOR gate
-        i_B => s_Operand, -- Input B for XOR gate
-        o_F => s_xorResult); -- Output of the XOR gate  (RT) 
+	G_XOR : nBitXor
+	PORT MAP(
+		i_A => i_RS,
+		i_B => s_Operand,
+		o_F => s_xorResult);
 
-    -- NOR gate operation
-    G_NOR : nBitNor
-    PORT MAP(
-        i_A => i_RS, -- Input A for NOR gate
-        i_B => s_Operand, -- Input B for NOR gate
-        o_F => s_norResult); -- Output of the NOR gate
+	G_NOR : nBitNor
+	PORT MAP(
+		i_A => i_RS,
+		i_B => s_Operand,
+		o_F => s_norResult);
 
-    -- SLT (Set Less Than) operation
-    G_SLT : setLessThan
-    PORT MAP(
-        i_A => i_RS, -- Input A for SLT operation
-        i_B => s_Operand, -- Input B for SLT operation
-        o_F => s_sltResult); -- Output of the SLT operation
+	G_SLT : setLessThan
+	PORT MAP(
+		i_A => i_RS,
+		i_B => s_Operand,
+		o_F => s_sltResult);
 
-    -- Add/Subtract operation
-    G_ADDSUB : nBitAddSub
-    PORT MAP(
-        input_A => i_RS, -- Input A for Add/Subtract operation
-        input_B => s_Operand, -- Input B for Add/Subtract operation
-        nAdd_Sub => i_addSub, -- Control signal for Add/Subtract
-        output_S => s_addSubResult, -- Output of Add/Subtract operation
-        output_C => s_addSubCarry, -- Carry output of Add/Subtract operation
-        o_Overflow => s_overflowDetected); -- Overflow signal from Add/Subtract operation
+	G_ADDSUB : nBitAddSub
+	PORT MAP(
+		input_A => i_RS,
+		input_B => s_Operand,
+		nAdd_Sub => i_addSub,
+		output_S => s_addSubResult,
+		output_C => s_addSubCarry,
+		o_Overflow => s_overflowDetected);
+	G_OVERFLOWAND : andg2 --only care about overflow flag if and only if the operation is not unsigned
+	PORT MAP(
+		i_A => s_overflowDetected,
+		i_B => i_signed,
+		o_F => o_overflow);
 
-    -- Overflow detection and setting the overflow output
-    G_OVERFLOWAND : andg2
-    PORT MAP(
-        i_A => s_overflowDetected, -- Overflow detected signal
-        i_B => i_signed, -- Signed operation control signal
-        o_F => o_overflow); -- Output overflow signal
+	G_SHIFT : Barrel_Shifter
+	PORT MAP(
+		i_shamt => s_shamt,
+		i_sign => s_barrelShiftSign,
+		i_leftShift => i_shiftDir,
+		i_D => s_Operand,
+		o_O => s_shiftResult);
 
-    -- Barrel shifter operation
-    G_SHIFT : Barrel_Shifter
-    PORT MAP(
-        i_shamt => s_shamt, -- Shift amount input to barrel shifter
-        i_sign => s_barrelShiftSign, -- Sign input to barrel shifter
-        i_leftShift => i_shiftDir, -- Direction of shift (left or right)
-        i_D => s_Operand, -- Data input to barrel shifter
-        o_O => s_shiftResult); -- Output of barrel shifter operation
+	G_BRANCH : branch
+	PORT MAP(
+		i_A => i_RS,
+		i_B => s_Operand,
+		i_bne => i_bne,
+		i_beq => i_beq,
+		o_branchFlag => o_branch);
 
-    -- Branch operation
-    G_BRANCH : branch
-    PORT MAP(
-        i_A => i_RS, -- Input A for branch operation
-        i_B => s_Operand, -- Input B for branch operation
-        i_bne => i_bne, -- Control signal for branch not equal
-        i_beq => i_beq, -- Control signal for branch equal
-        o_branchFlag => o_branch); -- Output branch flag
-
-    -- Selecting the ALU operation based on the ALUOp control signal
-    G_SELECT : selectOperation
-    PORT MAP(
-        i_ALUOp => i_ALUOp, -- ALU operation code
-        i_orResult => s_orResult, -- OR operation result
-        i_andResult => s_andResult, -- AND operation result
-        i_xorResult => s_xorResult, -- XOR operation result
-        i_norResult => s_norResult, -- NOR operation result
-        i_sltResult => s_sltResult, -- SLT operation result
-        i_addSubResult => s_addSubResult, -- Add/Subtract operation result
-        i_shiftResult => s_shiftResult, -- Shift operation result
-        o_result => o_result); -- ALU output result	    
-
-    G_EXTENDER : Extender
-    PORT MAP(
-        i_data => Imm,
-        o_out => s_signImm);
-
+	G_SELECT : selectOperation
+	PORT MAP(
+		i_ALUOp => i_ALUOp,
+		i_orResult => s_orResult,
+		i_andResult => s_andResult,
+		i_xorResult => s_xorResult,
+		i_norResult => s_norResult,
+		i_sltResult => s_sltResult,
+		i_addSubResult => s_addSubResult,
+		i_shiftResult => s_shiftResult,
+		o_result => o_result);
 
 END structure;
--- proj/src/TopLevel/ALU/nBitAnd.vhd
+++ proj/src/TopLevel/ALU/nBitAnd.vhd
@@ -1,34 +1,41 @@
---Abrahim Toutoungi
---3/17/2024
---nBitAnd.vhd
---To be used by the ALU for and & andi operations
+-------------------------------------------------------------------------
+-- Abrahim Toutoungi
+-- 3/14/2024
+-- nBitAnd.vhd
+-- To be used by the ALU for and & andi operations
+-------------------------------------------------------------------------
 
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 
 ENTITY nBitAnd IS
        PORT (
-              i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-              i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-              o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
+              i_A : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input A, 32-bit vector
+              i_B : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- Input B, 32-bit vector
+              o_F : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)); -- Output F, 32-bit vector
 END nBitAnd;
 
 ARCHITECTURE structure OF nBitAnd IS
 
+       -- Declare a component named andg2
        COMPONENT andg2 IS
               PORT (
-                     i_A : IN STD_LOGIC;
-                     i_B : IN STD_LOGIC;
-                     o_F : OUT STD_LOGIC);
+                     i_A : IN STD_LOGIC; -- Input A, single bit
+                     i_B : IN STD_LOGIC; -- Input B, single bit
+                     o_F : OUT STD_LOGIC); -- Output F, single bit
        END COMPONENT;
 
 BEGIN
+       -- Generate logic for 32-bit AND operation
        G_NBit_AND : FOR i IN 0 TO 31 GENERATE
 
+              -- Instantiate andg2 component for each bit
               NBitAnd : andg2
               PORT MAP(
-                     i_A => i_A(i),
-                     i_B => i_B(i),
-                     o_F => o_F(i));
+                     i_A => i_A(i), -- Connect the i-th bit of input A
+                     i_B => i_B(i), -- Connect the i-th bit of input B
+                     o_F => o_F(i)); -- Connect the i-th bit of output F
 
        END GENERATE G_NBit_AND;
+
+END structure;
