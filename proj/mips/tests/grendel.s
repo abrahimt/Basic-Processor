3af0e0ec33989eb3a36e6f74d0bb1effc86dea33
Abrahim Toutoungi <abrahimt@iastate.edu>
2024-04-29 13:44:36 -0500

    added nops to grendl

--- proj/mips/tests/grendel.s
+++ proj/mips/tests/grendel.s
@@ -23,335 +23,905 @@ visited:
 res_idx:
         .word   3
 .text
-	li $sp, 0x10011000
+	#li $sp, 0x10011000
+	lui $at, 0x1001
+	nop
+	nop
+	nop
+	ori $sp, $at, 0x00001000
 	li $fp, 0
-	la $ra pump
+	lasw $ra pump
 	j main # jump to the starting location
+	nop
+	nop
+	nop
 pump:
+	nop
+	nop
+	nop
 	halt
 
-
 main:
+	nop
+	nop
+	nop
         addiu   $sp,$sp,-40 # MAIN
+	nop
+	nop
+	nop
         sw      $31,36($sp)
         sw      $fp,32($sp)
         add    	$fp,$sp,$zero
+	nop
+	nop
+	nop
         sw      $0,24($fp)
         j       main_loop_control
+	nop
+	nop
+	nop
 
 main_loop_body:
+	nop
+	nop
+	nop
         lw      $4,24($fp)
-        la 	$ra, trucks
+        lasw 	$ra, trucks
         j     is_visited
+	nop
+	nop
+	nop
         trucks:
+	nop
+	nop
+	nop
 
         xori    $2,$2,0x1
+	nop
+	nop
+	nop
         andi    $2,$2,0x00ff
+        nop
+        nop
+        nop
         beq     $2,$0,kick
+	nop
+	nop
+	nop
 
         lw      $4,24($fp)
         # addi 	$k0, $k0,1# breakpoint
-        la 	$ra, billowy
+        lasw 	$ra, billowy
         j     	topsort
+	nop
+	nop
+	nop
         billowy:
+	nop
+	nop
+	nop
 
 kick:
+	nop
+	nop
+	nop
         lw      $2,24($fp)
+	nop
+	nop
+	nop
         addiu   $2,$2,1
+	nop
+	nop
+	nop
         sw      $2,24($fp)
+	nop
+	nop
+	nop
 main_loop_control:
+	nop
+	nop
+	nop
+
         lw      $2,24($fp)
+	nop
+	nop
+	nop
         slti     $2,$2, 4
+	nop
+	nop
+	nop
         beq	$2, $zero, hew # beq, j to simulate bne 
+	nop
+	nop
+	nop
         j       main_loop_body
+	nop
+	nop
+	nop
         hew:
+	nop
+	nop
+	nop
         sw      $0,28($fp)
         j       welcome
+	nop
+	nop
+	nop
 
 wave:
+	nop
+	nop
+	nop
         lw      $2,28($fp)
+	nop
+	nop
+	nop
         addiu   $2,$2,1
+	nop
+	nop
+	nop
         sw      $2,28($fp)
+	nop
+	nop
+	nop
 welcome:
+	nop
+	nop
+	nop
         lw      $2,28($fp)
+	nop
+	nop
+	nop
         slti    $2,$2,4
+	nop
+	nop
+	nop
         xori	$2,$2,1 # xori 1, beq to simulate bne where val in [0,1]
         beq     $2,$0,wave
+	nop
+	nop
+	nop
 
         move    $2,$0
         move    $sp,$fp
+	nop
+	nop
+	nop
         lw      $31,36($sp)
         lw      $fp,32($sp)
         addiu   $sp,$sp,40
         jr       $ra
+	nop
+	nop
+	nop
         
 interest:
+	nop
+	nop
+	nop
         lw      $4,24($fp)
-        la	$ra, new
+        lasw	$ra, new
         j	is_visited
+	nop
+	nop
+	nop
 	new:
+	nop
+	nop
+	nop
         xori    $2,$2,0x1
+	nop
+	nop
+	nop
         andi    $2,$2,0x00ff
+	nop
+	nop
+	nop
         beq     $2,$0,tasteful
-
+	nop
+	nop
+	nop
         lw      $4,24($fp)
-        la	$ra, partner
+        lasw	$ra, partner
         j     	topsort
+	nop
+	nop
+	nop
         partner:
+	nop
+	nop
+	nop
 
 tasteful:
+	nop
+	nop
+	nop
         addiu   $2,$fp,28
+	nop
+	nop
+	nop
         move    $4,$2
-        la	$ra, badge
+        lasw	$ra, badge
         j     next_edge
+	nop
+	nop
+	nop
         badge:
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
         sw      $2,24($fp)
+	nop
+	nop
+	nop
         
 turkey:
+	nop
+	nop
+	nop
         lw      $3,24($fp)
         li      $2,-1
+	nop
+	nop
+	nop
         beq	$3,$2,telling # beq, j to simulate bne
+	nop
+	nop
+	nop
         j	interest
+	nop
+	nop
+	nop
         telling:
-	la 	$v0, res_idx
+	nop
+	nop
+	nop
+	lasw 	$v0, res_idx
+	nop
+	nop
+	nop
 	lw	$v0, 0($v0)
         addiu   $4,$2,-1
-        la 	$3, res_idx
+        lasw 	$3, res_idx
+	nop
+	nop
+	nop
         sw 	$4, 0($3)
-        la	$4, res
+        lasw	$4, res
         #lui     $3,%hi(res_idx)
         #sw      $4,%lo(res_idx)($3)
         #lui     $4,%hi(res)
         sll     $3,$2,2
+	nop
+	nop
+	nop
         srl	$3,$3,1
+	nop
+	nop
+	nop
         sra	$3,$3,1
+	nop
+	nop
+	nop
         sll     $3,$3,2
+	nop
+	nop
+	nop
        
        	xor	$at, $ra, $2 # does nothing 
         nor	$at, $ra, $2 # does nothing 
         
-        la	$2, res
+        lasw	$2, res
+	nop
+	nop
+	nop
         andi	$at, $2, 0xffff # -1 will sign extend (according to assembler), but 0xffff won't
+	nop
+	nop
+	nop
         addu 	$2, $4, $at
+	nop
+	nop
+	nop
         addu    $2,$3,$2
+	nop
+	nop
+	nop
         lw      $3,48($fp)
+	nop
+	nop
+	nop
         sw      $3,0($2)
         move    $sp,$fp
+	nop
+	nop
+	nop
         lw      $31,44($sp)
         lw      $fp,40($sp)
         addiu   $sp,$sp,48
         jr      $ra
+	nop
+	nop
+	nop
    
 topsort:
+	nop
+	nop
+	nop
         addiu   $sp,$sp,-48
+	nop
+	nop
+	nop
         sw      $31,44($sp)
         sw      $fp,40($sp)
         move    $fp,$sp
+	nop
+	nop
+	nop
         sw      $4,48($fp)
         lw      $4,48($fp)
-        la	$ra, verse
+        lasw	$ra, verse
         j	mark_visited
+	nop
+	nop
+	nop
         verse:
+	nop
+	nop
+	nop
 
         addiu   $2,$fp,28
+	nop
+	nop
+	nop
         lw      $5,48($fp)
         move    $4,$2
-        la 	$ra, joyous
+        lasw 	$ra, joyous
         j	iterate_edges
+	nop
+	nop
+	nop
         joyous:
+	nop
+	nop
+	nop
 
         addiu   $2,$fp,28
+	nop
+	nop
+	nop
         move    $4,$2
-        la	$ra, whispering
+        lasw	$ra, whispering
         j     	next_edge
+	nop
+	nop
+	nop
         whispering:
+
+	nop
+	nop
+	nop
 
         sw      $2,24($fp)
         j       turkey
+	nop
+	nop
+	nop
 
 iterate_edges:
+	nop
+	nop
+	nop
         addiu   $sp,$sp,-24
+	nop
+	nop
+	nop
         sw      $fp,20($sp)
+	nop
+	nop
+	nop
         move    $fp,$sp
+	nop
+	nop
+	nop
         subu	$at, $fp, $sp
         sw      $4,24($fp)
         sw      $5,28($fp)
         lw      $2,28($fp)
+	nop
+	nop
+	nop
         sw      $2,8($fp)
         sw      $0,12($fp)
         lw      $2,24($fp)
         lw      $4,8($fp)
+	nop
+	nop
+	nop
         lw      $3,12($fp)
-        sw      $4,0($2)
+	nop
+	nop
+        sw      $4,0($2)    # I think this should be hazard free, it should be a simultaneous read/write
         sw      $3,4($2)
         lw      $2,24($fp)
         move    $sp,$fp
+	nop
+	nop
+	nop
         lw      $fp,20($sp)
         addiu   $sp,$sp,24
         jr      $ra
+	nop
+	nop
+	nop
         
 next_edge:
+	nop
+	nop
+	nop
         addiu   $sp,$sp,-32
+	nop
+	nop
+	nop
         sw      $31,28($sp)
         sw      $fp,24($sp)
+	nop
+	nop
+	nop
         add	$fp,$zero,$sp
+	nop
+	nop
+	nop
         sw      $4,32($fp)
         j       waggish
+	nop
+	nop
+	nop
 
 snail:
+	nop
+	nop
+	nop
         lw      $2,32($fp)
+	nop
+	nop
+	nop
         lw      $3,0($2)
         lw      $2,32($fp)
+	nop
+	nop
+	nop
         lw      $2,4($2)
+	nop
+	nop
+	nop
         move    $5,$2
         move    $4,$3
-        la	$ra,induce
+        lasw	$ra,induce
         j       has_edge
+	nop
+	nop
+	nop
         induce:
+	nop
+	nop
+	nop
         beq     $2,$0,quarter
+	nop
+	nop
+	nop
         lw      $2,32($fp)
+	nop
+	nop
+	nop
         lw      $2,4($2)
+	nop
+	nop
+	nop
         addiu   $4,$2,1
         lw      $3,32($fp)
+	nop
+	nop
+	nop
         sw      $4,4($3)
         j       cynical
+	nop
+	nop
+	nop
 
 
 quarter:
+	nop
+	nop
+	nop
         lw      $2,32($fp)
+	nop
+	nop
+	nop
         lw      $2,4($2)
+	nop
+	nop
+	nop
         addiu   $3,$2,1
         lw      $2,32($fp)
+	nop
+	nop
+	nop
         sw      $3,4($2)
 
 waggish:
+	nop
+	nop
+	nop
         lw      $2,32($fp)
+	nop
+	nop
+	nop
         lw      $2,4($2)
+	nop
+	nop
+	nop
         slti    $2,$2,4
+	nop
+	nop
+	nop
         beq	$2,$zero,mark # beq, j to simulate bne 
+	nop
+	nop
+	nop
         j	snail
+	nop
+	nop
+	nop
         mark:
+	nop
+	nop
+	nop
         li      $2,-1
 
 cynical:
+	nop
+	nop
+	nop
         move    $sp,$fp
+	nop
+	nop
+	nop
         lw      $31,28($sp)
         lw      $fp,24($sp)
         addiu   $sp,$sp,32
         jr      $ra
+	nop
+	nop
+	nop
 has_edge:
+	nop
+	nop
+	nop
         addiu   $sp,$sp,-32
+	nop
+	nop
+	nop
         sw      $fp,28($sp)
         move    $fp,$sp
+	nop
+	nop
+	nop
         sw      $4,32($fp)
         sw      $5,36($fp)
-        la      $2,adjacencymatrix
+        lasw      $2,adjacencymatrix
         lw      $3,32($fp)
+	nop
+	nop
+	nop
         sll     $3,$3,2
+	nop
+	nop
+	nop
         addu    $2,$3,$2
+	nop
+	nop
+	nop
         lw      $2,0($2)
+	nop
+	nop
+	nop
         sw      $2,16($fp)
         li      $2,1
+	nop
+	nop
+	nop
         sw      $2,8($fp)
         sw      $0,12($fp)
         j       measley
+	nop
+	nop
+	nop
 
 look:
+	nop
+	nop
+	nop
         lw      $2,8($fp)
+	nop
+	nop
+	nop
         sll     $2,$2,1
+	nop
+	nop
+	nop
         sw      $2,8($fp)
         lw      $2,12($fp)
+	nop
+	nop
+	nop
         addiu   $2,$2,1
-        sw      $2,12($fp)
+	nop
+	nop
+	nop
+        sw      $2,12($fp) # Should write to mem before the lw that follows reads mem. Actually has to
 measley:
+	nop
+	nop
+	nop
         lw      $3,12($fp)
         lw      $2,36($fp)
+	nop
+	nop
+	nop
         slt     $2,$3,$2
+	nop
+	nop
+	nop
         beq     $2,$0,experience # beq, j to simulate bne 
+	nop
+	nop
+	nop
         j 	look
+	nop
+	nop
+	nop
        	experience:
+	nop
+	nop
+	nop
         lw      $3,8($fp)
         lw      $2,16($fp)
+	nop
+	nop
+	nop
         and     $2,$3,$2
+	nop
+	nop
+	nop
         slt     $2,$0,$2
+	nop
+	nop
+	nop
         andi    $2,$2,0x00ff
         move    $sp,$fp
         lw      $fp,28($sp)
         addiu   $sp,$sp,32
         jr      $ra
+	nop
+	nop
+	nop
         
 mark_visited:
+	nop
+	nop
+	nop
         addiu   $sp,$sp,-32
+	nop
+	nop
+	nop
         sw      $fp,28($sp)
         move    $fp,$sp
+	nop
+	nop
+	nop
         sw      $4,32($fp)
         li      $2,1
+	nop
+	nop
+	nop
         sw      $2,8($fp)
         sw      $0,12($fp)
         j       recast
+	nop
+	nop
+	nop
 
 example:
+	nop
+	nop
+	nop
         lw      $2,8($fp)
+	nop
+	nop
+	nop
         sll     $2,$2,8
+	nop
+	nop
+	nop
         sw      $2,8($fp)
+	nop
+	nop
+	nop
         lw      $2,12($fp)
+	nop
+	nop
+	nop
         addiu   $2,$2,1
+	nop
+	nop
+	nop
         sw      $2,12($fp)
+	nop
+	nop
 recast:
+	nop
+	nop
+	nop
         lw      $3,12($fp)
         lw      $2,32($fp)
+	nop
+	nop
+	nop
         slt     $2,$3,$2
+	nop
+	nop
+	nop
         beq	$2,$zero,pat # beq, j to simulate bne
+	nop
+	nop
+	nop
         j	example
+	nop
+	nop
+	nop
         pat:
+	nop
+	nop
+	nop
 
-       	la	$2, visited
+       	lasw	$2, visited
+	nop
+	nop
+	nop
         sw      $2,16($fp)
         lw      $2,16($fp)
+	nop
+	nop
+	nop
         lw      $3,0($2)
         lw      $2,8($fp)
+	nop
+	nop
+	nop
         or      $3,$3,$2
         lw      $2,16($fp)
+	nop
+	nop
+	nop
         sw      $3,0($2)
         move    $sp,$fp
+	nop
+	nop
+	nop
         lw      $fp,28($sp)
         addiu   $sp,$sp,32
         jr      $ra
+	nop
+	nop
+	nop
         
 is_visited:
+	nop
+	nop
+	nop
         addiu   $sp,$sp,-32
+	nop
+	nop
+	nop
         sw      $fp,28($sp)
         move    $fp,$sp
+	nop
+	nop
+	nop
         sw      $4,32($fp)
         ori     $2,$zero,1
+	nop
+	nop
+	nop
         sw      $2,8($fp)
         sw      $0,12($fp)
         j       evasive
+	nop
+	nop
+	nop
 
 justify:
+	nop
+	nop
+	nop
         lw      $2,8($fp)
+	nop
+	nop
+	nop
         sll     $2,$2,8
+	nop
+	nop
+	nop
         sw      $2,8($fp)
         lw      $2,12($fp)
+	nop
+	nop
+	nop
         addiu   $2,$2,1
+	nop
+	nop
+	nop
         sw      $2,12($fp)
 evasive:
+	nop
+	nop
+	nop
         lw      $3,12($fp)
         lw      $2,32($fp)
+	nop
+	nop
+	nop
         slt     $2,$3,$2
+	nop
+	nop
+	nop
         beq	$2,$0,representitive # beq, j to simulate bne
+	nop
+	nop
+	nop
         j     	justify
+	nop
+	nop
+	nop
         representitive:
+	nop
+	nop
+	nop
 
-        la	$2,visited
+        lasw	$2,visited
+	nop
+	nop
+	nop
         lw      $2,0($2)
+	nop
+	nop
+	nop
         sw      $2,16($fp)
         lw      $3,16($fp)
         lw      $2,8($fp)
+	nop
+	nop
+	nop
         and     $2,$3,$2
+	nop
+	nop
+	nop
         slt     $2,$0,$2
+	nop
+	nop
+	nop
         andi    $2,$2,0x00ff
         move    $sp,$fp
+	nop
+	nop
+	nop
         lw      $fp,28($sp)
         addiu   $sp,$sp,32
         jr      $ra
+	nop
+	nop
+	nop
